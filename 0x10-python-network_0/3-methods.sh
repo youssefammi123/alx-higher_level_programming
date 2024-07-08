c77870082898ccfbcec9fc62ed0fbbf96fd5eb4d
youssefammi123 <149005042+youssefammi123@users.noreply.github.com>
2024-07-09 00:26:01 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+
+# Check if a URL was provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+# Send an OPTIONS request using curl and extract the Allow header
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d ' ' -f2-
