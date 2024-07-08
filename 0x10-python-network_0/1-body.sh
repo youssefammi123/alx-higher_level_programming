5e5bc7c24a8e67d50f23821729d694309d6c348f
youssefammi123 <149005042+youssefammi123@users.noreply.github.com>
2024-07-09 00:12:07 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,34 @@
-#!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+chmod +x 1-body.sh
+./1-body.sh http://0.0.0.0:5000/route_1
+./1-body.sh http://0.0.0.0:5000/route_1
+``
+
+./1-body.sh http://0.0.0.0:5000/route_1
+
+./1-body.sh http://0.0.0.0:5000/route_
+
+./1-body.sh http://0.0.0.0:5000/route
+
+./1-body.sh http://0.0.0.0:5000/
+
+./1-body.sh http://0.0.0.0:5000
+
+./1-body.sh http://0.0.0.0:
+
+./1-body.sh http://0.0.0.0
+
+./1-body.sh http://0.0.0
+
+./1-body.sh http://0.0
+
+./1-body.sh http://0
+
+./1-body.sh http://
+
+./1-body.sh http
+
+./1-body.sh
+
+./1-body
+
+./1
