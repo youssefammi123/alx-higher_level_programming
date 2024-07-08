3f64412a343b1dff540acd3b060b3df1a1b0bfa8
youssefammi123 <149005042+youssefammi123@users.noreply.github.com>
2024-07-09 00:07:43 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+
+# Check if a URL was provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+# Send a request using curl and display the size of the body of the response in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
